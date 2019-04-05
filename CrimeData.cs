845e54ec38d5bf63e9144715741ea44df1e1ba75
aamatetti <46973979+aamatetti@users.noreply.github.com>
2019-04-05 13:17:57 -0500

    Create CrimeData.cs

---
+++ CrimeData.cs
@@ -0,0 +1,14 @@
+namespace Data
+{
+public class CrimeData
+{
+public int Year { get; set; }
+public int Population { get; set; }
+public int Murders { get; set; }
+public int Rapes { get; set; }
+public int Robberies { get; set; }
+public int ViolentCrimes { get; set; }
+public int Thefts { get; set; }
+public int MotorVehicleThefts { get; set; }
+}
+}
